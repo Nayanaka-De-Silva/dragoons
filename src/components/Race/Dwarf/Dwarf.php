6d9792a5175c1b610d95189d46a7232ce5fdacb8
Nayanaka-De-Silva <k.nayanaka.desilva@gmail.com>
2023-10-02 02:28:41 +0800

    Added functionality to get Racial features
    The racial features themselves can obtained by getting the PlayerCharacters's Race property. The next step is to create a function to modify the actual score

--- src/components/Race/Dwarf/Dwarf.php
+++ src/components/Race/Dwarf/Dwarf.php
@@ -11,6 +11,7 @@ class Dwarf extends Race {
   private int $DEFAULT_AGE = 55;
   private float $DEFAULT_WEIGHT = 77.0; 
   private float $DEFAULT_HEIGHT = 4.0;
+  private array $abilityScoreIncrease = ['Type'=>'CON', 'Amount'=>2];
 
   private string $name = 'Dwarf';
 
@@ -30,5 +31,15 @@ class Dwarf extends Race {
 
   public function getDefaultHeight(){
     return $this->DEFAULT_HEIGHT;
+  }
+
+  // ---------------------------------------------------------------
+
+  public function getAbilityScoreIncreaseAmount() : int {
+    return $this->abilityScoreIncrease['Amount'];
+  }
+
+  public function getAbilityScoreIncreaseType() : string{
+    return $this->abilityScoreIncrease['Type'];
   }
 }
---
+++ tests/unit/DwarfTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Components\Test\Unit;
+
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+use PHPUnit\Framework\TestCase;
+use Components\Race\Dwarf\Dwarf;
+
+class DwarfTest extends TestCase {
+  
+
+  public function setUp(): void {
+    $this->testDwarf = new Dwarf();  
+  }
+  // ----------------------------------------------------------------
+  
+  public function testIfCanGetAbilityScoreIncreaseAmountForDwarfRace(): void {
+    $expectedAbilityScoreIncrease = 2;
+    $this->assertEquals($expectedAbilityScoreIncrease, $this->testDwarf->getAbilityScoreIncreaseAmount());
+  }
+
+  public function testIfCanGetAbilityScoreIncreaseTypeForDwarfRace(): void {
+    $expectedAbilityScoreIncreaseType = 'CON';
+    $this->assertEquals($expectedAbilityScoreIncreaseType, $this->testDwarf->getAbilityScoreIncreaseType());
+  }
+
+
+  // ----------------------------------------------------------------
+  public function tearDown(): void {
+    unset($this->testDwarf);
+  }
+}
