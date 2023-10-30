6351e070c327837042d7184d2b52714a0fa9eae2
Nayanaka-De-Silva <k.nayanaka.desilva@gmail.com>
2023-10-31 00:09:19 +0800

    Added ability to get spells as an array
    The next step for spells is to:
    1. Add it into the PlayerCharacter component
    2. Create the CLI so that the spellbook can be used as a feature
    
    For now, I just made it so that we don't have to get each individual attribute of the spell. An iterable array should be useful in this case.

--- src/components/Spell/Spell.php
+++ src/components/Spell/Spell.php
@@ -5,34 +5,34 @@ namespace Components\Spell;
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
 class Spell {
-	private string $name;
-	private string $desc;
-	private string $higher_level;
-	private string $page;
-	private string $range;
-	private array $components;
-	private bool 	$ritual;
-	private string $duration;
-	private bool $concentration;
-	private string $casting_time;
-	private int $level;
-	private string $school;
-	private array $classes;
+	private string 	$name;
+	private string 	$desc;
+	private string 	$higher_level;
+	private string 	$page;
+	private string 	$range;
+	private array 	$components;
+	private bool 		$ritual;
+	private string 	$duration;
+	private bool 		$concentration;
+	private string 	$casting_time;
+	private int 		$level;
+	private string 	$school;
+	private array 	$classes;
 
 	public function __construct(array $details) {
-		$this->name = $details['name'];
-		$this->desc = $details['desc'];
-		$this->higher_level = $details['higher_level'] ?? '';
-		$this->page = $details['page'];
-		$this->range = $details['range'];
-		$this->components = explode(", ", $details['components']);
-		$this->ritual = $details['ritual'] != "no" ? true : false;
-		$this->duration = $details['duration'];
-		$this->concentration = $details['concentration'] != "no" ? true : false;
-		$this->casting_time = $details['casting_time'];
-		$this->level = $details['level'] != "Cantrip" ? $details['level'][0] : 0;
-		$this->school = $details['school'];
-		$this->classes = explode(", ", $details['classes']);
+		$this->name 					= $details['name'];
+		$this->desc 					= $details['desc'];
+		$this->higher_level 	= $details['higher_level'] ?? '';
+		$this->page 					= $details['page'];
+		$this->range 					= $details['range'];
+		$this->components 		= explode(", ", $details['components']);
+		$this->ritual 				= $details['ritual'] != "no" ? true : false;
+		$this->duration 			= $details['duration'];
+		$this->concentration 	= $details['concentration'] != "no" ? true : false;
+		$this->casting_time 	= $details['casting_time'];
+		$this->level 					= $details['level'] != "Cantrip" ? $details['level'][0] : 0;
+		$this->school 				= $details['school'];
+		$this->classes 				= explode(", ", $details['classes']);
 	}
 
 	public function getName() : string {
@@ -85,5 +85,23 @@ class Spell {
 
 	public function getClasses() : array {
 		return $this->classes;
+	}
+
+	public function getSpellAsArray() : array {
+		return array(
+			'name' 					=> $this->getName(),
+			'desc' 					=> $this->getDesc(),
+			'higher_level' 	=> $this->getHigherLevel(),
+			'page' 					=> $this->getPage(),
+			'range' 				=> $this->getRange(),
+			'components' 		=> $this->getComponents(),
+			'ritual' 				=> $this->getRitual(),
+			'duration' 			=> $this->getDuration(),
+			'concentration' => $this->getConcentration(),
+			'casting_time' 	=> $this->getCastingTime(),
+			'level' 				=> $this->getLevel(),
+			'school' 				=> $this->getSchool(),
+			'classes' 			=> $this->getClasses()
+		);
 	}
 }
