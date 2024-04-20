1a9ab6b9fffdff9abc12a68cf30863df9c6195d8
Nayanaka-De-Silva <k.nayanaka.desilva@gmail.com>
2024-04-21 00:45:04 +0800

    Added the changes to the directory

--- src/components/Feature/Darkvision.php
+++ src/components/Feature/Darkvision.php
@@ -4,14 +4,14 @@ namespace Components\Traits;
 
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
-use Components\Traits\Traits;
+use Components\Traits\Trait;
 
 
 /**
  * Darkvision
  * 
  */
-class Darkvision extends Traits {
+class Darkvision implements Trait {
 	public function getDescription() {
 		return "Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bgith light, and in darkness as if it were dim light. You can't discen color in darkness, only shades of grey.";
 	}
--- src/components/Feature/Trance.php
+++ src/components/Feature/Trance.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Components\Traits;
+namespace Components\Feature;
+
+use Components\Feature\Feature;
 
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
@@ -8,7 +10,7 @@ require_once(__DIR__ . '/../../../vendor/autoload.php');
  * Trance
  * 
  */
-class Trance extends Traits {
+class Trance implements Feature {
 	public function getDescription() {
 		return "Elves don’t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is “trance.”) While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years o f practice. After resting in this way, you gain the same benefit that a human does from 8 hours o f sleep.";
 	}
